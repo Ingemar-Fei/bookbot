ebe5e3233358eea28d6f8b68308fa6d1ff00a7b3
pedro-cheng <pedro-cheng@outlook.com>
2025-05-19 20:21:19 +0800

    Show report

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import get_num_characters
+import stats
 
 def get_book_text(path_to_file:str):
     with open(path_to_file) as f:
@@ -6,9 +6,20 @@ def get_book_text(path_to_file:str):
         file_contents = f.read()
     return file_contents
 
+def print_report(path_to_file):
+    contents = get_book_text(path_to_file)
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {path_to_file}...")
+    print("----------- Word Count ----------")
+    num_words = stats.get_num_words(contents)
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    dict_num_words = stats.get_num_characters(contents)
+    list_num_words = stats.sort_num_characters(dict_num_words)
+    for i in list_num_words:
+        print(f"{i["name"]}: {i["num"]}")
+    print("============= END ===============")
 def main():
-    contents = get_book_text(path_to_file="./books/frankenstein.txt")
-    num_words = get_num_characters(contents)
-    print(num_words)
-
+    print_report(path_to_file="books/frankenstein.txt")
+    
 main()
--- stats.py
+++ stats.py
@@ -10,3 +10,13 @@ def get_num_characters(contents:str):
         else:
             num_char[i] += 1
     return num_char
+
+def sort_num_characters(dict_char:dict):
+    def sort_on(dict):
+        return dict['num']
+    list_char_num = []
+    for char in dict_char:
+        if char.isalpha():
+            list_char_num.append({"name":char,"num":dict_char[char]})
+    list_char_num.sort(reverse=True,key=sort_on)
+    return list_char_num 
